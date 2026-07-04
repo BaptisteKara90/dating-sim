0f15eabc227ca17d89d86250e3dd2e1381d45661
BaptisteKara90 <baptiste.kara@gmail.com>
2026-07-04 23:57:46 +0200

    refacto type

--- scripts/loaders/DialogueLoader.gd
+++ scripts/loaders/DialogueLoader.gd
@@ -4,19 +4,19 @@ extends RefCounted
 const DialogueScript = preload("res://scripts/models/Dialogue.gd")
 
 func load_dialogue(dialogue_name: String):
-	var path := "res://data/dialogues/%s.json" % dialogue_name
+	var path: String = "res://data/dialogues/%s.json" % dialogue_name
 
 	if not FileAccess.file_exists(path):
 		push_error("Dialogue file not found : " + path)
 		return DialogueScript.new()
 
-	var file := FileAccess.open(path, FileAccess.READ)
+	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
 
 	if file == null:
 		push_error("Unable to open dialogue : " + path)
 		return DialogueScript.new()
 
-	var json := file.get_as_text()
+	var json: String = file.get_as_text()
 
 	var parsed = JSON.parse_string(json)
 
